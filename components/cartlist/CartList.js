f13108666a17b782e1a72857ed52d0fb9724ee57
gmlron001 <gmlron001@myuct.ac.za>
2021-07-13 21:28:06 +0200

    cart margin fix

--- components/cartlist/CartList.js
+++ components/cartlist/CartList.js
@@ -29,14 +29,14 @@ export default function CartList() {
                      <div className="flex-shrink-0 px-6 py-6 sm:px-6 sticky z-20 bottom-0 w-full right-0 left-0 bg-accent-0 border-t text-sm">
                           <ul className="pb-2">
                               <li className="flex justify-between py-1">
-                                <span>Subtotal</span><span>$329.99</span>
+                                <span>Subtotal</span><span> ZAR 32499</span>
                               </li>
                               <li className="flex justify-between py-1">
                                 <span>Taxes</span>
-                                <span>Included in item price</span>
+                                <span>ZAR 4200</span>
                               </li><li className="flex justify-between py-1">
-                              <span>Shipping</span><span className="font-bold tracking-wide">ZAR 55</span>
-                              </li></ul><div className="flex justify-between border-t border-accent-2 py-3 font-bold mb-2"><span>Total</span><span>$329.99</span></div><div>
+                              <span>Shipping</span><span className="font-bold tracking-wide">ZAR 100</span>
+                              </li></ul><div className="flex justify-between border-t border-accent-2 py-3 font-bold mb-2"><span>Total</span><span>ZA 37299</span></div><div>
                               <button className="bg-black font-medium text-white"  style={{width: "100%",height:40}}>Proceed to Checkout</button>
                               </div></div>
             </div>
