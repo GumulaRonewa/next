80a5b27a390256d5bde107b2cc72955e81ce7b78
GumulaRonewa <44128309+GumulaRonewa@users.noreply.github.com>
2021-07-16 08:40:24 +0200

    Update Home.js

--- components/landing/Home.js
+++ components/landing/Home.js
@@ -4,8 +4,8 @@ export default function Home(props) {
 
 	return(
     <div className="first">
-      <video autoPlay playsInline id="video-background" muted loop  className="video">
-        <source src="https://cdn.videvo.net/videvo_files/video/free/2015-03/small_watermarked/Water_9_150fps_videvo_preview.webm" type="video/webm"/>
+      <video autoPlay  preload={true} id="video-background" muted loop  className="video">
+        <source src="https://cdn.videvo.net/videvo_files/video/free/2015-03/small_watermarked/Water_9_150fps_videvo_preview.webm" type='video/webm; codecs="vp8.0, vorbis"' />
       </video>
       <Link href="/shop">
         <button className="shoping">
