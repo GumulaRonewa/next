47525467913b6721af189824869bbdb21f11a409
GumulaRonewa <44128309+GumulaRonewa@users.noreply.github.com>
2021-07-16 08:15:59 +0200

    Update Home.js

--- components/landing/Home.js
+++ components/landing/Home.js
@@ -4,7 +4,7 @@ export default function Home(props) {
 
 	return(
     <div className="first">
-      <video autoPlay={true} muted loop  className="video">
+      <video autoPlay playsInline id="video-background" muted loop  className="video">
         <source src="https://cdn.videvo.net/videvo_files/video/free/2015-03/small_watermarked/Water_9_150fps_videvo_preview.webm" type="video/webm"/>
       </video>
       <Link href="/shop">
