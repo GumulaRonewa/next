6215c1c4f100b2ae2a63ac331d5595370e677bb6
gmlron001 <gmlron001@myuct.ac.za>
2021-07-16 05:14:05 +0200

    landing page styling 3

--- components/landing/Home.js
+++ components/landing/Home.js
@@ -7,9 +7,11 @@ export default function Home(props) {
       <video autoPlay={true} muted loop  className="video">
         <source src="https://cdn.videvo.net/videvo_files/video/free/2015-03/small_watermarked/Water_9_150fps_videvo_preview.webm" type="video/webm"/>
       </video>
+      <Link href="/shop">
         <button className="shoping">
          Shop Now
          </button>
+      </Link>
     </div>
 		)
 }
