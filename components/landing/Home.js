a53cb39312a5fbe5fe16dce4ee68e728e19d90ec
GumulaRonewa <44128309+GumulaRonewa@users.noreply.github.com>
2021-07-16 09:37:19 +0200

    Update Home.js

--- components/landing/Home.js
+++ components/landing/Home.js
@@ -6,7 +6,7 @@ export default function Home(props) {
 	return(
     <div className="first">
       <div className="video">
-              <Image  layout="fill" className=" mb-10" objectFit="cover" src={item.img} alt="" />
+              <Image  layout="fill" className=" mb-10" objectFit="cover" src={"https://cdn.zmescience.com/wp-content/uploads/2017/03/drops-of-water-578897_1280-1024x682.jpg"} alt="" />
       </div>
       <Link href="/shop">
         <button className="shoping">
