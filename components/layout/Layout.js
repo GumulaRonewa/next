4180866ee1ef4e8a7464b632c1f686e1f8ac3fba
gmlron001 <gmlron001@myuct.ac.za>
2021-07-16 05:25:09 +0200

    landing page styling 4

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -3,7 +3,7 @@ import classes from './Layout.module.css';
 
 function Layout(props) {
   return (
-    <div className="divide-y divide-fuchsia-300">
+    <div className="divide-y divide-fuchsia-300" style={{zIndex:1}}>
       <MainNavigation />
       <main className={classes.main}>{props.children}</main>
     </div>
