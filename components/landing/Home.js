95a5bddcb4e94af1f12e9a3cd5f42cb015f89c67
gmlron001 <gmlron001@myuct.ac.za>
2021-07-17 21:03:53 +0200

    align

--- components/landing/Home.js
+++ components/landing/Home.js
@@ -8,14 +8,16 @@ export default function Home(props) {
       <div className="video">
               <Image  layout="fill" className=" mb-10" objectFit="cover" src={"https://wokiee.reactdemo.hasthemes.com/assets/images/slides/01/slide-01.jpg"} alt="" />
       </div>
-      <div className="captions">
-       Premium
+       <div className="placecenter">
+         <div className="captions">
+       Premium,Exclusive Clothes
       </div>
       <Link href="/shop">
         <button className="shoping">
          Shop Now
          </button>
       </Link>
+       </div>
     </div>
 		)
 }
